a92b44230cd01704772677a8ec593269f90605aa
ivangianni <ivangianni@live.it>
2026-03-13 18:55:17 +0100

    Implemented solid check on diaglib results

--- test/test_fortran/test_fortran.f90
+++ test/test_fortran/test_fortran.f90
@@ -6,17 +6,9 @@ program test_fortran
     use direct_matvecs
     use utility
     implicit none
-    integer, parameter :: n = 500, n_targ = 5, n_max = 10, max_iter = 100, dav_iter = 10
-    logical :: verbose = .false.
-    real(dp), parameter :: tol = 1.0e-10_dp, shift = 1.0_dp
-    integer, parameter :: memory = 100
-    character(len=2), parameter :: memory_unit = "MB"
-    procedure(), pointer :: mx_p => null()
-!
     logical :: ok
     real(dp), allocatable :: eig(:), evec(:, :), evec_2(:, :)
-    integer, parameter :: lutest = 100
-    character(len=30) :: results_fname = "output_fortran.txt"
+    character(len=20), parameter :: f_string = "('-- ', a, /)"
 !
 ! open a text file for the output, to be used to compare the results with a reference.
 !
@@ -36,7 +28,7 @@ program test_fortran
 !
     call init_eigenpairs(n, n_max, eig, evec, ok)
 !
-    write (6, *) ' testing Davidson:'
+    write (6, f_string) 'testing Davidson:'
     call dgl_davidson_driver(n, n_targ, n_max, ax, dx, eig, evec, ok, &
                              dgl_verbose=verbose, &
                              dgl_max_iter=max_iter, &
@@ -48,17 +40,23 @@ program test_fortran
                              )
 !
     if (ok) then
-        write (6, *) ' Davidson converged.'
+        write (6, f_string) 'Davidson converged.'
+        eig = eig - shift
+        ok = compare_eigs(n, n_targ, tol, eig, evec, "Symmetric diagonalization")
         call dump_eigpairs(lutest, n, n_targ, eig, evec, "Davidson")
+        if (.not. ok) then
+            write (*, f_string) "Davidson results do not match with reference!"
+            !stop
+        end if
     else
-        write (6, *) ' Davidson failed to converge.'
+        write (6, f_string) 'Davidson failed to converge.'
     end if
 !
 ! test generalized davidson:
 !
     call init_eigenpairs(n, n_max, eig, evec, ok)
 !
-    write (6, *) ' testing Generalized Davidson:'
+    write (6, f_string) 'testing Generalized Davidson:'
     call dgl_davidson_driver(n, n_targ, n_max, ax, dx, eig, evec, ok, metvec=mx_p, &
                              dgl_verbose=verbose, &
                              dgl_max_iter=max_iter, &
@@ -70,10 +68,16 @@ program test_fortran
                              )
 !
     if (ok) then
-        write (6, *) ' Generalized Davidson converged.'
+        write (6, f_string) 'Generalized Davidson converged.'
+        ok = compare_eigs(n, n_targ, tol, eig, evec, "Symmetric Generalized")
         call dump_eigpairs(lutest, n, n_targ, eig, evec, "Generalized Davidson")
+        if (.not. ok) then
+            write (*, f_string) "Generalized Davidson results do not match with reference!"
+            !stop
+        end if
+
     else
-        write (6, *) 'Generalized Davidson failed to converge.'
+        write (6, f_string) 'Generalized Davidson failed to converge.'
     end if
 !
 ! test non-symmetric davidson:
@@ -81,7 +85,7 @@ program test_fortran
     call init_eigenpairs(n, n_max, eig, evec, ok)
     call init_eigenpairs(n, n_max, eig, evec_2, ok)
 !
-    write (6, *) ' testing non-symmetric Davidson:'
+    write (6, f_string) 'testing non-symmetric Davidson:'
     call dgl_davidson_nosym_driver(n, n_targ, n_max, arx, alx, dx, "LR", eig, evec, ok, evec_2=evec_2, &
                                    dgl_verbose=verbose, &
                                    dgl_max_iter=max_iter, &
@@ -93,18 +97,30 @@ program test_fortran
                                    )
 !
     if (ok) then
-        write (6, *) ' non-symmetric Davidson converged.'
+        write (6, f_string) 'non-symmetric Davidson converged.'
+        eig = eig - shift
+        ok = compare_eigs(n, n_targ, tol, eig, evec, "Non Symmetric diagonalization, Right")
         call dump_eigpairs(lutest, n, n_targ, eig, evec, "Non Symmetric Davidson, Right")
+        if (.not. ok) then
+            write (*, f_string) "Right Non symmetric Davidson results do not match with reference!"
+            !stop
+        end if
+
+        ok = compare_eigs(n, n_targ, tol, eig, evec_2, "Non Symmetric diagonalization, Left")
         call dump_eigpairs(lutest, n, n_targ, eig, evec_2, "Non Symmetric Davidson, Left")
+        if (.not. ok) then
+            write (*, f_string) "Left Non symmetric Davidson results do not match with reference!"
+            !stop
+        end if
     else
-        write (6, *) ' non-symmetric Davidson failed to converge.'
+        write (6, f_string) 'non-symmetric Davidson failed to converge.'
     end if
 !
 ! test lobpcg:
 !
     call init_eigenpairs(n, n_max, eig, evec, ok)
 !
-    write (6, *) ' testing LOBPCG:'
+    write (6, f_string) 'testing LOBPCG:'
 
     call dgl_lobpcg_driver(n, n_targ, n_max, ax, dx, eig, evec, ok, &
                            dgl_verbose=verbose, &
@@ -116,17 +132,23 @@ program test_fortran
                            )
 !
     if (ok) then
-        write (6, *) ' LOBPCG converged.'
+        write (6, f_string) 'LOBPCG converged.'
+        eig = eig - shift
+        ok = compare_eigs(n, n_targ, tol, eig, evec, "Symmetric diagonalization")
         call dump_eigpairs(lutest, n, n_targ, eig, evec, "LOBPCG")
+        if (.not. ok) then
+            write (*, f_string) "LOBPCG results do not match with reference!"
+            !stop
+        end if
     else
-        write (6, *) ' LOBPCG failed to converge.'
+        write (6, f_string) 'LOBPCG failed to converge.'
     end if
 !
 ! test generalized lobpcg:
 !
     call init_eigenpairs(n, n_max, eig, evec, ok)
 !
-    write (6, *) ' testing Generalized LOBPCG:'
+    write (6, f_string) 'testing Generalized LOBPCG:'
 
     call dgl_lobpcg_driver(n, n_targ, n_max, ax, dx, eig, evec, ok, metvec=mx_p, &
                            dgl_verbose=verbose, &
@@ -138,10 +160,15 @@ program test_fortran
                            )
 !
     if (ok) then
-        write (6, *) ' Generalized LOBPCG converged.'
+        write (6, f_string) 'Generalized LOBPCG converged.'
+        ok = compare_eigs(n, n_targ, tol, eig, evec, "Symmetric Generalized")
         call dump_eigpairs(lutest, n, n_targ, eig, evec, "Generalized LOBPCG")
+        if (.not. ok) then
+            write (*, f_string) "Generalized LOBPCG results do not match with reference!"
+            !stop
+        end if
     else
-        write (6, *) ' Generalized LOBPCG failed to converge.'
+        write (6, f_string) 'Generalized LOBPCG failed to converge.'
     end if
 !
 ! test smogd:
@@ -150,7 +177,7 @@ program test_fortran
     allocate (evec(2*n, n_max))
     call init_eigenpairs(2*n, n_max, eig, evec, ok)
 !
-    write (6, *) ' testing SMOGD:'
+    write (6, f_string) 'testing SMOGD:'
     call dgl_smogd_driver(2*n, n_targ, n_max, apbx, ambx, spdx, smdx, lrprc, &
                           eig, evec, ok, &
                           dgl_verbose=verbose, &
@@ -161,10 +188,15 @@ program test_fortran
                           dgl_memory_unit=memory_unit &
                           )
     if (ok) then
-        write (6, *) ' SMOGD converged.'
+        write (6, f_string) 'SMOGD converged.'
+        ok = compare_eigs(2*n, n_targ, tol, eig, evec, "Linear response")
         call dump_eigpairs(lutest, n*2, n_targ, eig, evec, "SMOGD")
+        if (.not. ok) then
+            write (*, f_string) "SMOGD results do not match with reference!"
+            !stop
+        end if
     else
-        write (6, *) ' SMOGD failed to converge.'
+        write (6, f_string) 'SMOGD failed to converge.'
     end if
 !
 ! close the output file:
--- test/test_fortran/utility.f90
+++ test/test_fortran/utility.f90
@@ -1,8 +1,22 @@
 module utility
-
+!!
+!! Common module for testing
+!!
     integer :: i, j
     integer, parameter :: dp = selected_real_kind(15)
     real(dp), parameter :: zero = 0._dp, one = 1._dp, two = 2._dp, five = 5._dp
+
+    integer, parameter :: n = 500, n_targ = 5, n_max = 20
+    integer, parameter :: max_iter = 100, dav_iter = 10
+    logical :: verbose = .false.
+    real(dp), parameter :: tol = 1.0e-10_dp, shift = 0.0_dp
+    integer, parameter :: memory = 100
+    character(len=2), parameter :: memory_unit = "MB"
+    procedure(), pointer :: mx_p => null()
+
+    character(len=30) :: reference_fname = "output_reference.txt"
+    character(len=30) :: results_fname = "output_fortran.txt"
+    integer, parameter :: lutest = 100, luref = 101
 
     interface prtmat
         module procedure prtmat_r
@@ -12,63 +26,129 @@ module utility
 contains
 
 !
-    subroutine init_eigenpairs(n, n_max, eig, evec, ok)
+    subroutine init_eigenpairs(ld, n_eig, eig, evec, ok)
 !!
 !! Make a simple guess
 !!
         implicit none
-        integer, intent(in) :: n
-        integer, intent(in) :: n_max
-        real(dp) :: eig(n_max)
-        real(dp) :: evec(n, n_max)
+        integer, intent(in) :: ld
+        integer, intent(in) :: n_eig
+        real(dp) :: eig(n_eig)
+        real(dp) :: evec(ld, n_eig)
         logical :: ok
 !
         eig = zero
         evec = zero
-        do i = 1, n_max
+        do i = 1, n_eig
             evec(i, i) = one
         end do
         ok = .false.
 
     end subroutine init_eigenpairs
 
-    subroutine dump_eigpairs(unit, n, n_targ, eig, evec, string)
+    logical function compare_eigs(ld, n_eig, thresh, eig, evec, string) result(success)
+        integer, intent(in) :: ld, n_eig
+        real(dp), intent(in) :: thresh
+        real(dp), intent(in) :: eig(n_eig), evec(ld, n_eig)
+        character(len=*) :: string
+
+        real(dp), allocatable :: ex_eig(:), ex_evec(:, :), norms(:, :)
+
+        success = .true.
+
+        allocate (ex_eig(n_eig), ex_evec(ld, n_eig), norms(n_eig, 2))
+
+        call read_reference(ld, n_eig, ex_eig, ex_evec, string)
+
+        if (maxval(eig - ex_eig) .gt. thresh) success = .false.
+        do i = 1, n_eig
+            norms(i, 1) = sqrt(dot_product(evec(:, i), evec(:, i)))
+            norms(i, 2) = sqrt(dot_product(ex_evec(:, i), ex_evec(:, i)))
+        end do
+
+        if (maxval(norms(:, 1) - norms(:, 2)) .gt. thresh) success = .false.
+
+        if (.not. success) then
+            write (*, "(t3,a,*(d14.4))") "Comp Eigenvals: ", eig
+            write (*, "(t3,a,*(d14.4))") "Ref  Eigenvals: ", ex_eig
+            write (*, "(t3,a,*(d14.4))") "Diff Eigenvals: ", ex_eig - eig
+            write (*, *)
+            write (*, "(t3,a,*(d14.4))") "Norm of Comp Eigenvecs: ", norms(:, 1)
+            write (*, "(t3,a,*(d14.4))") "Norm of Ref  Eigenvecs: ", norms(:, 2)
+            write (*, "(t3,a,*(d14.4))") "Norm of Diff Eigenvecs: ", norms(:, 2) - norms(:, 1)
+        end if
+
+        deallocate (ex_eig, ex_evec, norms)
+
+    end function compare_eigs
+
+    subroutine read_reference(ld, n_eig, eig, evec, string)
+        integer, intent(in) :: ld, n_eig
+        real(dp), intent(inout) :: eig(n_eig), evec(ld, n_eig)
+        character(len=*), intent(in) :: string
+
+        character(len=200) :: line
+        integer :: k
+
+        if (.not. file_exist(trim(reference_fname))) then
+            write (*, "(t3,a)") "Reference file does not exist, first run the 'reference' executable"
+            stop
+        end if
+
+        open (file=trim(reference_fname), unit=luref, status="old")
+
+        do
+            read (luref, "(a)") line
+            if (index(line, string) .ne. 0) exit
+        end do
+
+        read (luref, *)
+        read (luref, *)
+        do i = 1, n_eig
+            read (luref, *) k, eig(i)
+        end do
+
+        read (luref, *)
+        do i = 1, ld
+            read (luref, *) k, (evec(i, j), j=1, n_eig)
+        end do
+
+        close (luref)
+
+    end subroutine read_reference
+
+    subroutine dump_eigpairs(unit, ld, n_eig, eig, evec, string)
 !!
 !! dump eigvecs to outputfile to be compares with a reference.
 !!
         implicit none
-        integer, intent(in) :: n
-        integer, intent(in) :: n_targ
+        integer, intent(in) :: ld
+        integer, intent(in) :: n_eig
         integer, intent(in) :: unit
         character(len=*), intent(in) :: string
-        real(dp), intent(inout) :: eig(n_targ), evec(n, n_targ)
+        real(dp), intent(inout) :: eig(n_eig), evec(ld, n_eig)
 
-        logical :: opened
-        character(len=5) :: s_unit
-
-        inquire (unit=unit, opened=opened)
-        if (.not. opened) then
-            write (s_unit, "(i0)") unit
-            write (*, *) "Writing on closed file unit "//s_unit
+        if (.not. file_opened(unit)) then
+            write (*, "(t3,a)") "Tried to dump on file that is not open"
             stop
         end if
 
         write (unit, 1000) string
         write (unit, *)
         write (unit, 1010)
-        do i = 1, n_targ
+        do i = 1, n_eig
             write (unit, 1020) i, eig(i)
         end do
 !
 ! Fix the phase
 !
-        do i = 1, n_targ
+        do i = 1, n_eig
             if (evec(1, i) .lt. zero) evec(:, i) = -evec(:, i)
         end do
 
-        write (unit, 1030) (i, i=1, n_targ)
-        do j = 1, n
-            write (unit, 1022) j, (evec(j, i), i=1, n_targ)
+        write (unit, 1030) (i, i=1, n_eig)
+        do j = 1, ld
+            write (unit, 1022) j, (evec(j, i), i=1, n_eig)
         end do
         write (unit, *)
 !
@@ -79,37 +159,55 @@ contains
 1030    format(t3, 'Eigenvectors ', *(i16, 6x))
 !
     end subroutine dump_eigpairs
-!
-    subroutine prtmat_r(n, m, mat)
+
+    logical function file_exist(fname)
         implicit none
-        integer, intent(in) :: n, m
-        real(dp), dimension(n, m), intent(in) :: mat
+        character(len=*), intent(in) :: fname
+
+        inquire (file=fname, exist=file_exist)
+
+    end function file_exist
+!
+    logical function file_opened(unit)
+        implicit none
+        integer, intent(in) :: unit
+
+        inquire (unit=unit, opened=file_opened)
+
+    end function file_opened
+
+    subroutine prtmat_r(ld, m, mat)
+        implicit none
+        integer, intent(in) :: ld, m
+        real(dp), dimension(ld, m), intent(in) :: mat
 
         character(len=5) :: s_n, s_m
         character(len=20) :: fmt
-        integer :: i, j
-        write (s_n, "(i0)") n
+
+        write (s_n, "(i0)") ld
         write (s_m, "(i0)") m
         fmt = "("//s_m//"d12.3)"
-        do i = 1, n
+        do i = 1, ld
             write (*, trim(fmt)) (mat(i, j), j=1, m)
         end do
+
     end subroutine prtmat_r
 !
-    subroutine prtmat_i(n, m, mat)
+    subroutine prtmat_i(ld, m, mat)
         implicit none
-        integer, intent(in) :: n, m
-        integer, dimension(n, m), intent(in) :: mat
+        integer, intent(in) :: ld, m
+        integer, dimension(ld, m), intent(in) :: mat
 
         character(len=5) :: s_n, s_m
         character(len=20) :: fmt
-        integer :: i, j
-        write (s_n, "(i0)") n
+
+        write (s_n, "(i0)") ld
         write (s_m, "(i0)") m
         fmt = "("//s_m//"i4)"
-        do i = 1, n
+        do i = 1, ld
             write (*, trim(fmt)) (mat(i, j), j=1, m)
         end do
+
     end subroutine prtmat_i
 !
 end module utility
